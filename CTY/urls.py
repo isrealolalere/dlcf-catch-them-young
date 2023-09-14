664b0867b78a9f64e07a8d4ded13e169f5835341
isrealolalere <isrealolalere09@gmail.com>
2023-09-14 15:00:38 +0100

    modified the urlpatterns in urls.py

--- CTY/urls.py
+++ CTY/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dlcf-cty/', include('dlcf_online.urls')),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_rooot=settings.STATICFILES_DIRS[0])
+
